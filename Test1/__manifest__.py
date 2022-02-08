e23415de6ee841d2e4a1a0e23bf9533fa5014f92
Juanma <juanmanuel.c95@gmail.com>
2022-02-08 19:52:24 +0000

    [ADD] Basic Module

---
+++ Test1/__manifest__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*
+{
+    'name': 'ModuloTest',
+    'summary': """""Academy app yo manage Training""",
+    'description': """""
+        Descripcion del modulo.
+    """,
+    'author':'Odoo',
+    'Wbsite': 'https://www.odoo.com',
+    'category': 'Training',
+    'version': '0.1',
+    'depends': ['base'],
+    'data': [
+        
+    ],
+    
+    'demo':[
+        
+    ],
+    
+    
+}
